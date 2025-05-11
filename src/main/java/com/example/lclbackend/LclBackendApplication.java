963aa172b3c77774900b6bf546b0570b6f184d7a
ymtdik <user@example.com>
2025-05-11 18:54:11 +0900

    initial commit

---
+++ src/main/java/com/example/lclbackend/LclBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.lclbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LclBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LclBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lclbackend/LclBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lclbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LclBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
